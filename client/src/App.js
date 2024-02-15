8226bbc876232dcb6dc40a2273a09fe294e564a8
Tejas <tejassuresh07@gmail.com>
2024-02-15 22:25:48 +0530

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import FAQ from "./components/pages/FAQ";
+import Contact from "./components/pages/Contact";
+import GovtOrg from "./components/pages/GovtOrg";
+import Login from "./components/auth.jwt/Login";
+import UserDash from "./components/pages/User_Dash";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about-us" element={<About />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="contact-us" element={<Contact />} />
+          <Route path="govt-organization" element={<GovtOrg />} />
+          <Route path="user-dashbord" element={<UserDash/>}/>
+        </Route>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
