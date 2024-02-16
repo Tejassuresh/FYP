8a3152c5a9025f9b439af622cb62498ccdc012c5
Tejas <tejassuresh07@gmail.com>
2024-02-16 09:52:58 +0530

    some changes in 16-02-2024 morning

--- client/src/components/auth.jwt/Login.jsx
+++ client/src/components/auth.jwt/Login.jsx
@@ -74,7 +74,7 @@ const Login = () => {
               <TextField
                 variant="standard"
                 name="email"
-                label="Enter mail-id"
+                label="Enter User-id"
               />
               <TextField
                 variant="standard"
